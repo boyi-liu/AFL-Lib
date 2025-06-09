70faade42436de0d9715d067a93795e02ab05881
boyi-liu <byliu0325@gmail.com>
2025-06-09 17:45:00 +0800

    fix bug

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 class FedSim:
     def __init__(self, args):
         self.args = args
-
+        args.suffix = f'exp/{args.suffix}'
         if not os.path.exists(f'./{args.suffix}'):
             os.makedirs(f'./{args.suffix}')
 
