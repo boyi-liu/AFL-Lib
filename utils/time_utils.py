2be37dcff965c1be4d867be2f085d85c92bd7370
boyi-liu <byliu0325@gmail.com>
2025-07-11 11:40:26 +0800

    update time_utils.py with dropout

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,4 +1,6 @@
 import time
+import random
+
 from functools import wraps
 
 def time_record(func):
@@ -9,5 +11,9 @@ def time_record(func):
         end_time = time.time()
         execution_time = end_time - start_time
         self.training_time = execution_time * self.delay
+
+        dropout = self.sysconfig['dropout']
+        if dropout['dropout'] and random.random() < dropout['drop_prob']:
+            self.training_time += (random.random() * dropout['drop_latency'])
         return result
     return wrapper
